e9de34e82e6cdba54035a2b1f6f1bf0adde0eca0
Kevin04C <kevincespedes2004@gmai.com>
2022-11-15 22:10:14 -0500

    add store App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router'
+import { store } from './store/store'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 }
 
 export default App

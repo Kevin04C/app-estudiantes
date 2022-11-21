7af64bd5a7d2e9795975624a5329919ed921e916
Kevin04C <kevincespedes2004@gmai.com>
2022-11-20 21:31:08 -0500

    feat: add custom useForm.jsx

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useForm = (initialForm) => {
+  const [stateForm, setStateForm] = useState(initialForm)
+
+  const reset = () => {
+    setStateForm(initialForm)
+  }
+
+  const handleInputChange = ({ target }) => {
+    setStateForm({
+      ...stateForm,
+      [target.name]: target.value
+    })
+  }
+
+  return {
+    stateForm,
+    ...stateForm,
+
+    reset,
+    handleInputChange
+  }
+}

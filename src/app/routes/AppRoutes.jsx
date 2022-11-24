5e5d97f249eef8137da5ab388ba616cd8e6545dd
Kevin04C <kevincespedes2004@gmai.com>
2022-11-24 16:11:04 -0500

    feat: add  HomePage route how index

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -3,11 +3,14 @@ import { CalculadoraPage } from '../pages/CalculadoraPage';
 import { CoursePage } from '../pages/CoursePage';
 import { AppLayout } from '../layout/AppLayout';
 import { TodoPages } from '../pages/TodoPage';
+import React from 'react';
+import { HomePage } from '../pages/HomePage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<AppLayout />}>
+        <Route index element={<HomePage />}/>
         <Route path='todos' element={<TodoPages />} />
         <Route path='courses' element={<CoursePage />} />
         <Route path='calculator' element={<CalculadoraPage />} />

a65be6c7616075ac3270ceab0bef49afd5a9b63c
Kevin04C <kevincespedes2004@gmai.com>
2022-11-24 16:10:04 -0500

    feat: show name username of the studient

--- src/app/components/header/ProfilePopUp.jsx
+++ src/app/components/header/ProfilePopUp.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// icons
-// https://react-icons.github.io/react-icons/icons?name=im
 import {
   BsBoxArrowLeft,
   BsFillFilePersonFill,
   BsLightningFill,
   BsPatchQuestionFill,
 } from 'react-icons/bs';
+import { useDispatch, useSelector } from 'react-redux';
+import { onLogout } from '../../../store/auth/authSlice';
 
 const ProfilePopUp = ({ refProfile }) => {
+  const dispatch = useDispatch();
+  const { name, username } = useSelector((state) => state.auth.user);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    dispatch(onLogout());
+  };
+
   return (
     <div ref={refProfile} className='profile-pop'>
       <div className='profile-pop__header'>
@@ -19,8 +27,8 @@ const ProfilePopUp = ({ refProfile }) => {
           alt=''
         />
         <div className='profile-pop__user-info'>
-          <h2 className='profile-pop__name'>Rodrigo Carpio</h2>
-          <h2 className='profile-pop__user'>@rodcarpio</h2>
+          <h2 className='profile-pop__name'>{name}</h2>
+          <h2 className='profile-pop__user'>@{username}</h2>
         </div>
       </div>
       <div className='profile-pop__body'>
@@ -37,7 +45,7 @@ const ProfilePopUp = ({ refProfile }) => {
           Algo más
         </Link>
       </div>
-      <div className='profile-pop__footer'>
+      <div className='profile-pop__footer' onClick={handleLogout}>
         <Link className='profile-pop__link'>
           <BsBoxArrowLeft />
           Cerrar sesión

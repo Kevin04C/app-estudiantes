3cd5009868ed5343e30fb6b6de1c8fa390b62bca
Kevin04C <kevincespedes2004@gmai.com>
2022-12-06 17:08:50 -0500

    perf: change in the code to keep the user

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -7,25 +7,53 @@ export const authSlice = createSlice({
       'not-checking' /* 'authenticated' 'not-authenticated', 'checking' */,
     user: {},
     errorMessage: '',
+    successMessage: '',
+    loadingApp: false,
   },
   reducers: {
     onChecking: (state) => {
       state.status = 'checking';
     },
+    onCloseChecking: (state) => {
+      state.status = 'not-authenticated';
+    },
+    onLoadingApp: (state) => {
+      state.loadingApp = true;
+    },
+    onCloseLoadingApp: (state) => {
+      state.loadingApp = false;
+    },
     onLogin: (state, { payload }) => {
       state.status = 'authenticated';
       state.user = payload;
+      state.loadingApp = false;
     },
     onLogout: (state, { payload }) => {
       state.status = '';
       state.user = {};
+      state.loadingApp = false;
       state.errorMessage = payload || '';
     },
     clearErrorMessage: (state) => {
       state.errorMessage = '';
     },
+    setSuccessMessage: (state, {payload}) => {
+      state.successMessage = payload;
+    },
+    clearSuccessMessage: (state) => {
+      state.successMessage = '';
+    }
   },
 });
 
-export const { onChecking, onLogin, onLogout, clearErrorMessage } =
-  authSlice.actions;
+export const {
+  clearErrorMessage,
+  onChecking,
+  onCloseChecking,
+  onCloseLoadingApp,
+  onLoadingApp,
+  onLogin,
+  onLogout,
+  setSuccessMessage,
+  clearSuccessMessage,
+} = authSlice.actions;

8617d3b8ab29b625fbb13b72e91a91c08101bddd
franz <gavino_10@hotmail.es>
2023-01-13 13:59:44 -0500

    feat: create components-chores

--- src/app/components/chores/Chores.jsx
+++ src/app/components/chores/Chores.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import { Body, Header } from "./components";
 
-export const Chores = () => {
+
+export const Chores=()=> {
+
+  const [view, setview] = useState(false);
+   
+
+
   return (
     <>
-        <h1>Coursoss</h1>
+      <div className="container">
+        <Header setview={setview} />
+        <Body/>
+        {
+          view && (<div  className="container__tareas " id="container__tareas">
+          <div className="container__tareas__form" id="form">
+            <a onClick={()=>setview(false)} className="container__tareas__form__close"><i>cerrar</i></a>
+            <h1 className="container__tareas__form__titulo">REGISTRAR TAREAS</h1>
+            <form className="container__tareas__form__form" action="">
+              <input className="container__tareas__form__form__input" type="text" placeholder="CURSO:" />
+              <input className="container__tareas__form__form__input" type="text" placeholder="TAREA"/>
+              <input className="container__tareas__form__form__input" type="text" placeholder="CONTENIDO:" />
+              <label className="container__tareas__form__form__label" htmlFor="">Fecha de inicio:</label>
+              <input className="container__tareas__form__form__input" type="date"/>
+              <label className="container__tareas__form__form__label" htmlFor="">Fecha de fin:</label>
+              <input className="container__tareas__form__form__input" type="date"/>
+              <button className="container__tareas__form__form__button">Registrar tarea</button>
+            </form>
+          </div>
+        </div>)
+        }
+      </div>
     </>
   );
 };

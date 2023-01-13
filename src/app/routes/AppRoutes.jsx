8f45acbd37d090708d00d7bf3cd7b197d86380e1
franz <gavino_10@hotmail.es>
2023-01-13 13:52:36 -0500

    feat:create new route-chores

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -3,8 +3,8 @@ import ArticlesPage from '../pages/Articles/ArticlesPage';
 import { CalculadoraPage } from '../pages/CalculadoraPage';
 import { CoursePage } from '../pages/Courses/CoursePage';
 import { AppLayout } from '../layout/AppLayout';
-import { TodoPages } from '../pages/TodoPage';
 import { HomePage } from '../pages/HomePage';
+import { Chores } from '../components/chores/Chores';
 
 export const AppRoutes = () => {
   return (
@@ -12,11 +12,10 @@ export const AppRoutes = () => {
       <Route path='/' element={<AppLayout />}>
         <Route element={<HomePage />} />
         <Route path='search=:search' element={<ArticlesPage />} />
-        <Route path='todos' element={<TodoPages />} />
+        <Route path='chores' element={<Chores />} />
         <Route path='courses/*' element={<CoursePage />}>
           <Route path='new' element={<h1>Nuevo curso</h1>} />
         </Route>
-        <Route path='todos' element={<TodoPages />} />
         <Route path='search' element={<ArticlesPage />} />
         <Route path='calculator' element={<CalculadoraPage />} />
       </Route>

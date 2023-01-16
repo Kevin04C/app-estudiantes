c4e2b284c823cab79f16d6f697519cb42e4546f8
franz <gavino_10@hotmail.es>
2023-01-16 14:26:37 -0500

    feat: agregando estilos al contenedor del body

--- src/app/components/chores/components/BodyContainer.jsx
+++ src/app/components/chores/components/BodyContainer.jsx
@@ -1,47 +1,31 @@
 import React from 'react';
 
-export const BodyContainer = () => {
+
+export const BodyContainer = ({dataChores}) => {
 
   const urlTareas=`../../../../../public/img/ecommerce.png`;
+
+
+
   return (
     <>
+        
         <div className="front-body__container">
               <p className="front-body__container__titulo">Listado de tareas</p>
-              <div className="front-body__container__contenido">
-                <div className="front-body__container__contenido__img">
-                  <img className="front-body__container__contenido__img-img" src={urlTareas} alt="imagen" />
-                </div>
-                <div className="front-body__container__contenido__text">
-                  <small className="front-body__container__contenido__text__small">Programacion</small>
-                  <h1 className="front-body__container__contenido__text__titulo">Desarrollar vistas de un Ecommerce</h1>
-                  <p className="front-body__container__contenido__text__contenido">Crear las vistas de las pantallas que vera el usuario final del aplicativo ecommerce</p>
-                </div>
-                
-              </div>
-              
-              <div className="front-body__container__contenido">
-                <div className="front-body__container__contenido__img">
-                  <img className="front-body__container__contenido__img-img" src={urlTareas} alt="imagen" />
-                </div>
-                <div className="front-body__container__contenido__text">
-                  <small className="front-body__container__contenido__text__small">Programacion</small>
-                  <h1 className="front-body__container__contenido__text__titulo">Desarrollar vistas de un Ecommerce</h1>
-                  <p className="front-body__container__contenido__text__contenido">Crear las vistas de las pantallas que vera el usuario final del aplicativo ecommerce</p>
-                </div>
-                
-              </div>
-
-              <div className="front-body__container__contenido">
-                <div className="front-body__container__contenido__img">
-                  <img className="front-body__container__contenido__img-img" src={urlTareas} alt="imagen" />
-                </div>
-                <div className="front-body__container__contenido__text">
-                  <small className="front-body__container__contenido__text__small">Programacion</small>
-                  <h1 className="front-body__container__contenido__text__titulo">Desarrollar vistas de un Ecommerce</h1>
-                  <p className="front-body__container__contenido__text__contenido">Crear las vistas de las pantallas que vera el usuario final del aplicativo ecommerce</p>
-                </div>
-                
-              </div>
+                {
+                  dataChores.map((d)=>(
+                    <div key={d.curso} className="front-body__container__contenido">
+                        <div className="front-body__container__contenido__img">
+                          <img className="front-body__container__contenido__img-img" src={urlTareas} alt="imagen" />
+                        </div>
+                        <div className="front-body__container__contenido__text">
+                          <small className="front-body__container__contenido__text__small">{d.curso}</small>
+                          <h1 className="front-body__container__contenido__text__titulo">{d.tarea}</h1>
+                          <p className="front-body__container__contenido__text__contenido">{d.contenido}</p>
+                        </div>   
+                      </div>
+                  ))
+                }
           </div>
     </>
   );

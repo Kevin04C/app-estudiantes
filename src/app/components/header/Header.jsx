3fec982fb077aa8d161bb10ac2587a10b73da900
Kevin04C <kevincespedes2004@gmail.com>
2023-01-18 18:53:46 -0500

    feat: remove Notifacation of the Header

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,18 +1,16 @@
 import { Link } from 'react-router-dom';
 import Profile from './Profile';
-import Notification from './Notification';
 import Searcher from './Searcher';
 const Header = () => {
   return (
     <header className='header'>
       <div className='header__left'>
-        <Link to='/home' className='header__logo'>
+        <Link to='/' className='header__logo'>
           <span>App</span> Student
         </Link>
         <Searcher />
       </div>
       <div className='header__right'>
-        <Notification />
         <Profile />
       </div>
     </header>

120b95b2b8cf61b3a33b0b8bafad19811cbc1862
Nicolás Díaz <nikodiaz56@gmail.com>
2023-01-19 19:06:39 -0300

    feat: add welcome card

---
+++ src/app/components/Home/CourseModule.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CourseModule = () => {
+  return (
+    <section className='courses'>
+      <div>
+        <h1>Nuevos Cursos</h1>
+        <a>Ver más</a>
+      </div>
+    </section>
+  );
+};
+
+export default CourseModule;

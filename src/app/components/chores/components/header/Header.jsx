e6ce12c971842961c7932a5c1a968e9fb64c9759
franz <gavino_10@hotmail.es>
2023-01-18 22:10:18 -0500

    feat: creando la carpeta header para ordenar archivos

---
+++ src/app/components/chores/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HeaderTareas } from './HeaderTareas';
+
+export const Header = ({setview}) => {
+
+  const urlImg=`../../../../../public/img/welcome.png`;
+
+  return (
+    <>
+        <div className="front-cabecera">
+
+          <div className="front-cabecera__welcome">
+
+              <div className="front-cabecera__welcome__1">
+
+                <h1 className="front-cabecera__welcome__1__titulo">Buenos Dias,<small className="front-cabecera__welcome__1__titulo__small">FRANZITO</small></h1>
+                <p className="front-cabecera__welcome__1__contenido">Bienvenido a la seccion de tareas, aqui podras selecionar que tareas deseas realizar.</p>
+                <p className="front-cabecera__welcome__1__hora">11:00am</p>
+                
+              </div>
+
+              <img className='front-cabecera__welcome__img' src={urlImg} alt="welcome" />
+
+          </div>
+
+          <HeaderTareas setview={setview} />
+
+        </div>
+    </>
+  );
+};
+
+
---
+++ src/app/components/chores/components/header/HeaderTareas.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const HeaderTareas = ({setview}) => {
+
+  const mostrarFormulario=()=>{
+    setview(true);
+  };
+
+  return (
+    <>
+        <div className="front-cabecera__tareas">
+
+              <h1 className="front-cabecera__tareas__titulo">Tareas</h1>
+
+              <button 
+              onClick={mostrarFormulario} 
+              id='btn_tareas' 
+              className="front-cabecera__tareas__button">
+              Crear Tareas
+              </button>
+
+        </div>
+    </>
+  );
+};

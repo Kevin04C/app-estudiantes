487f9554a78f4d14e91fa8ae7a420f065e28284c
Kevin04C <kevincespedes2004@gmail.com>
2023-01-22 16:15:49 -0500

    feat: add uiSlice

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './auth/authSlice';
 import searchSlice from './search/searchSlice';
 import articleSlice from './articles/articleSlice';
+import { uiSlice } from './ui/uiSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     search: searchSlice.reducer,
     articles: articleSlice.reducer,
+    ui: uiSlice.reducer,
   },
 });

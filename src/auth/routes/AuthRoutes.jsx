45937a672e1d5533bcfd4465e3c9a9c28173dbe1
Kevin04C <kevincespedes2004@gmail.com>
2023-01-28 18:30:35 -0500

    feat: recover password user

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
+import { RecoverAccount } from '../pages/RecoverAccount';
+import { RecoverPassword } from '../pages/RecoverPassword';
 import { RegisterPage } from '../pages/RegisterPage';
 
 export const AuthRoutes = () => {
@@ -7,6 +9,8 @@ export const AuthRoutes = () => {
     <Routes>
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
+      <Route path='/recover-account' element={<RecoverAccount />} />
+      <Route path='/recover-password' element={<RecoverPassword />} />
 
       <Route path='/*' element={<Navigate to='/auth/login' />} />
     </Routes>

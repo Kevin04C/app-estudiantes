8fb2cf454b0fd7593a44718350d58e5dc298e001
Kevin04C <kevincespedes2004@gmail.com>
2023-01-27 23:15:19 -0500

    feat: update commets with data of faker.js

--- src/app/components/landing/Comment.jsx
+++ src/app/components/landing/Comment.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 
-export const Comment = ({ name, role, avatar, text }) => {
+export const Comment = ({ role, text }) => {
   return (
     <article className='comment-card'>
       <div className='comment-card__head'>
-        <img src={avatar} alt='' className='comment-card__avatar' />
+        <img src={faker.image.avatar()} alt='' className='comment-card__avatar' />
         <div className='comment-card__user'>
-          <h3 className='comment-card__user__name'>{name}</h3>
+          <h3 className='comment-card__user__name'>{faker.name.fullName()}</h3>
           <p className='comment-card__user__student'>{role}</p>
         </div>
       </div>

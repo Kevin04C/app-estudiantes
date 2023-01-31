f8733fa055bc7e0049371785de3dc09fb7325d6b
Nicolás Díaz <nikodiaz56@gmail.com>
2023-01-31 18:02:20 -0300

    refactor: historical search saves in server

--- src/app/components/Course/CourseFilter.jsx
+++ src/app/components/Course/CourseFilter.jsx
@@ -2,30 +2,27 @@ import React from 'react';
 
 const CourseFilter = ({ setAll, setPending, setInProgress, setComplete, show }) => {
   return (
-    <div className='courses-filter'>
-      <button
-        className={`courses-filter__btn ${show === 'all' || '' ? 'active' : ''}`}
-        onClick={setAll}
-      >
+    <div className='filter'>
+      <button className={`filter__btn ${show === 'all' || '' ? 'active' : ''}`} onClick={setAll}>
         Todos
       </button>
       <span>|</span>
       <button
-        className={`courses-filter__btn ${show === 'pending' || '' ? 'active' : ''}`}
+        className={`filter__btn ${show === 'pending' || '' ? 'active' : ''}`}
         onClick={setPending}
       >
         Pendientes
       </button>
       <span>|</span>
       <button
-        className={`courses-filter__btn ${show === 'inProgress' || '' ? 'active' : ''}`}
+        className={`filter__btn ${show === 'inProgress' || '' ? 'active' : ''}`}
         onClick={setInProgress}
       >
         Cursando
       </button>
       <span>|</span>
       <button
-        className={`courses-filter__btn ${show === 'complete' || '' ? 'active' : ''}`}
+        className={`filter__btn ${show === 'complete' || '' ? 'active' : ''}`}
         onClick={setComplete}
       >
         Completado
